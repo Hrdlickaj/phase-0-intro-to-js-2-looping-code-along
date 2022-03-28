7797e045faef043d54c1cb9937f9e68ba110b75d
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-03-28 09:47:15 -0600

    commit tree 868f3a76612cee16b409dd798c847dfa0861f318

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function countDown(number) {
+  let countdown = number; 
+  while (countdown >= 0) {
+    console.log(countdown--);
+  }
+}
+
+const nameList = [""]
+const messageList = []
+
+function writeCards(name, event) {
+  for (let i = 0; i <= nameList.length; i++) {
+  console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+  return messageList;
+}
